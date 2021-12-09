19e8649fa9e0a8a3b955a0ff6eedee6de30e5466
tiiedye <tiiedye@gmail.com>
2021-12-09 13:18:39 -0800

    big fixes and refactoring

--- snake.py
+++ snake.py
@@ -3,6 +3,10 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -20,16 +24,20 @@ class Snake:
             self.segments.append(new_segment)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
     def move(self):
         # Has segments follow head
