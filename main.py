46ea49359d736b27e75083ec4abdf12740f9f856
tiiedye <tiiedye@gmail.com>
2021-12-09 12:16:51 -0800

    setting up game screen

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Snake project
+import turtle
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+# All code before exit
+screen.exitonclick()
