c95c37a57d38a158368e2a8a7fab4cc5436e1a8c
tiiedye <tiiedye@gmail.com>
2021-12-10 10:21:03 -0800

    refactoring score

--- score.py
+++ score.py
@@ -9,9 +9,12 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 275)
         self.score = 0
+        self.update_score()
+
+    def update_score(self):
         self.write(f"Score: {self.score}", False, align="center", font=("Arial", 16, "normal"))
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", False, align="center", font=("Arial", 16, "normal"))
+        self.update_score()
