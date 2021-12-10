92f9739d5e341cba90c960a3d49b9d68f074e4ea
tiiedye <tiiedye@gmail.com>
2021-12-10 09:35:32 -0800

    Making a Food class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
