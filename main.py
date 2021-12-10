d6f3787c96f86c5246a93853cd5c17de325ee89b
tiiedye <tiiedye@gmail.com>
2021-12-10 09:39:38 -0800

    adding collision detection to food/snake.head

--- main.py
+++ main.py
@@ -26,5 +26,9 @@ while game_is_on:
 
     snake.move()
 
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
+
 # All code before exit
 screen.exitonclick()
